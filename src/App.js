fe2422defd92386ab256388dae0bf316ad4f620b
PIYUSH NEGI <43876655+npiyush97@users.noreply.github.com>
2022-02-04 19:17:29 +0530

    'minorfix'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Die from "./component/Die";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
@@ -6,95 +6,99 @@ import Confetti from "react-confetti";
 import "./App.css";
 
 export default function App() {
-    let date = Date.now();
-    let fastesttime = localStorage.getItem("fastest");
-    const [count, setCount] = useState(0);
-    const [dice, setDice] = React.useState(allNewDice());
-    const [tenzies, setTenzies] = React.useState(false);
-    const [startTime, setTime] = useState(date);
-    const [fastest, setfastest] = useState(fastesttime || 0);
-
-    React.useEffect(() => {
-        const allHeld = dice.every((die) => die.isHeld);
-        const firstValue = dice[0].value;
-        const allSameValue = dice.every((die) => die.value === firstValue);
-        if (allHeld && allSameValue) {
-            setTenzies(true);
-            let time = Math.floor((Date.now() - startTime) / 1000);
-            if (time < fastest) {
-                setfastest(time);
-                localStorage.setItem("fastest", time);
-            }
-            setTime(time);
-        }
-    }, [dice]);
-    function generateNewDie() {
-        return {
-            value: Math.ceil(Math.random() * 6),
-            isHeld: false,
-            id: nanoid(),
-        };
+  let fastesttime = localStorage.getItem("fastest");
+  console.log(fastesttime);
+  const [count, setCount] = useState(0);
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+  const [startTime, setTime] = useState(Date.now());
+  const [fastest, setfastest] = useState(fastesttime || 0);
+  let time = useRef();
+  React.useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      let latest = Date.now();
+      time.current = Math.floor((latest - startTime) / 1000);
+      if (fastest == 0) {
+        localStorage.setItem("fastest", time);
+      }
+      if (time < fastest) {
+        setfastest(time);
+        console.log("called");
+        localStorage.setItem("fastest", time);
+      }
     }
+  }, [dice]);
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
-    function allNewDice() {
-        const newDice = [];
-        for (let i = 0; i < 10; i++) {
-            newDice.push(generateNewDie());
-        }
-        return newDice;
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
     }
+    return newDice;
+  }
 
-    function rollDice() {
-        if (!tenzies) {
-            setCount((count) => count + 1);
-            setDice((oldDice) =>
-                oldDice.map((die) => {
-                    return die.isHeld ? die : generateNewDie();
-                })
-            );
-        } else {
-            setCount(0);
-            setTenzies(false);
-            setDice(allNewDice());
-        }
+  function rollDice() {
+    if (!tenzies) {
+      setCount((count) => count + 1);
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setCount(0);
+      setTenzies(false);
+      setDice(allNewDice());
     }
+  }
 
-    function holdDice(id) {
-        setDice((oldDice) =>
-            oldDice.map((die) => {
-                return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-            })
-        );
-    }
-
-    const diceElements = dice.map((die) => (
-        <Die
-            key={die.id}
-            value={die.value}
-            isHeld={die.isHeld}
-            holdDice={() => holdDice(die.id)}
-        />
-    ));
-
-    return (
-        <main>
-            {tenzies && <Confetti />}
-            <h1 className="title">Tenzies</h1>
-            <div className="time-section">
-                <span className="floating-watch">
-                    Time : {tenzies ? startTime : 0} sec{" "}
-                </span>
-                <span className="floating-timer">Fastest : {fastest} sec </span>
-            </div>
-            <p className="instructions">
-                Roll until all dice are the same. Click each die to freeze it at
-                its current value between rolls.
-            </p>
-            <div className="dice-container">{diceElements}</div>
-            <div>No of rolls : {count}</div>
-            <button className="roll-dice" onClick={rollDice}>
-                {tenzies ? "New Game" : "Roll"}
-            </button>
-        </main>
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
     );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <div className="time-section">
+        <span className="floating-watch">
+          Time : {tenzies ? time.current : 0} sec{" "}
+        </span>
+        <span className="floating-timer">Fastest : {fastest} sec </span>
+      </div>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <div>No of rolls : {count}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
+  );
 }
